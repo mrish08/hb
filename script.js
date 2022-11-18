97941f0543b3507a3c67367fedb97b97ece7b876
mrish08 <muyright22@gmail.com>
2022-11-18 15:00:58 +0800

    s

--- script.js
+++ script.js
@@ -22,10 +22,10 @@ let updateTimer;
 
 const music_list = [
     {
-        img : 'images/stay.png',
-        name : 'Stay',
-        artist : 'The Kid LAROI, Justin Bieber',
-        music : 'music/stay.mp3'
+        img : 'images/al1.png',
+        name : 'With a Smiles',
+        artist : 'Eraserheads',
+        music : 'music/with_a_smile_cover_by_kaleigh.mp3'
     },
     {
         img : 'images/fallingdown.jpg',
@@ -33,18 +33,7 @@ const music_list = [
         artist : 'Wid Cards',
         music : 'music/fallingdown.mp3'
     },
-    {
-        img : 'images/faded.png',
-        name : 'Faded',
-        artist : 'Alan Walker',
-        music : 'music/Faded.mp3'
-    },
-    {
-        img : 'images/ratherbe.jpg',
-        name : 'Rather Be',
-        artist : 'Clean Bandit',
-        music : 'music/Rather Be.mp3'
-    }
+    
 ];
 
 loadTrack(track_index);
